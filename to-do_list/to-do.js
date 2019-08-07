f4194a6a58c5ba46d1ef8e55abd6cfb095e79cf5
Syra-Nyth <48935704+Syra-Nyth@users.noreply.github.com>
2019-08-08 10:05:35 +1200

    to-do_list v.3
    delete task

--- to-do_list/to-do.js
+++ to-do_list/to-do.js
@@ -1,21 +1,34 @@
 $(function() {
-    let todoList = []
+    let list = []
 
     $(`#btnAddTask`).click(function() {
         // get an item from the user
         let task = $(`#txtTask`).val()
 
         // add the item to the list
-        todoList.push(task)
+        list.push(task)
 
     }) // end button
     
+
+    $(`#btnDelTask`).click(function() {
+        // get an task number from the user
+        let index = $(`#numDelTask`).val()
+        // convert the task number to a number
+        index = parseInt(index)
+
+        // starting at index delete 1 task
+        list.splice(index, 1)
+
+    }) // end button
+
+
     $(`#btnPrintList`).click(function() {
         // start the list fresh
         $(`#lstTodoList`).empty()
 
         // for every list item add a <li>
-        todoList.forEach(function(value, index) {
+        list.forEach(function(value, index) {
             $(`#lstTodoList`).append(`<li>Task Number ${index}: ${value}</li>`)
 
         })
